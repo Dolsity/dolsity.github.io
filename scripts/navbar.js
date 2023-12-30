3225aea7c0cf50a56cffe31baaafd45269a361b0
Jay <Acorder43812@gmail.com>
2023-12-29 21:26:30 -0800

    Update navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,21 +2,28 @@ const menu = document.querySelector('#mobile_menu');
 const menuLinks = document.querySelector('.navbar_menu');
 const navLogo = document.querySelector('#navbar_logo');
 
-const mobileMenu = () => {
+window.addEventListener("load", (event) => {
+    console.log("page is fully loaded");
+    if (window.innerWidth > 850 && window.scrollY < 570) {
+        const homeMenu = document.querySelector('#home_section');
+        homeMenu.classList.add('highlight_link');
+    }
+});
+
+menu.addEventListener('click', () => {
+    console.log('clicked');
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-};
+});
 
-menu.addEventListener('click', mobileMenu);
-
-const highlightMenuOnScroll = () => {
+window.addEventListener('scroll', () => {
     const elem = document.querySelector('.highlight_link');
     const homeMenu = document.querySelector('#home_section');
     const skillMenu = document.querySelector('#skills_section');
     const ProjectMenu = document.querySelector('#projects_section');
     let scrollPos = window.scrollY;
 
-    if (window.innerWidth > 850 && scrollPos < 575) {
+    if (window.innerWidth > 850 && scrollPos < 570) {
         skillMenu.classList.remove('highlight_link');
         ProjectMenu.classList.remove('highlight_link');
         homeMenu.classList.add('highlight_link');
@@ -33,40 +40,24 @@ const highlightMenuOnScroll = () => {
     if (elem && window.innerWidth < 850 && scrollPos < 575) {
         elem.classList.remove('highlight_link');
     }
-};
+});
 
-const highlightMenuOnClick = (event) => {
+window.addEventListener('click', (event) => {
     const target = event.target;
     const navbarLinks = document.querySelectorAll('.navbar_links');
 
-    navbarLinks.forEach((link) => {
-        link.classList.remove('highlight_link');
-    }
-);
-
-if (target.classList.contains('navbar_links')) {
+    if (target.classList.contains('navbar_links')) {
         target.classList.add('highlight_link');
+        navbarLinks.forEach((link) => {
+            link.classList.remove('highlight_link');
+        });
     }
-};
-
-window.addEventListener('scroll', highlightMenuOnScroll);
-window.addEventListener('click', highlightMenuOnClick);
+});
 
 // Close mobile menu when clicking on a menu item
 const hideMobileMenu = (event) => {
-    const menuBars = document.querySelector('.is-active');
-    const target = event.target;
-
-    // Check if the clicked element is a navbar link
-    if (target.classList.contains('navbar_links')) {
-        menu.classList.remove('is-active');
-        menuLinks.classList.remove('active');
-    }
-
-    if (window.innerWidth <= 850 && menuBars && !target.classList.contains('navbar_container')) {
-        menu.classList.remove('is-active');
-        menuLinks.classList.remove('active');
-    }
+    menu.classList.remove('is-active');
+    menuLinks.classList.remove('active');
 };
 
 // Close mobile menu when clicking outside of the menu
