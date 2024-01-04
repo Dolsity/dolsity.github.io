c23c2cde8d570ca35016701bcba09766e3836b3f
Jay <Acorder43812@gmail.com>
2024-01-04 13:22:25 -0800

    Fixed pause time for typewriter

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -8,15 +8,18 @@ $(document).ready(function() {
         .typeString(
             '<span class="purple">import</span> <span class="tan">Human</span>'
         )
+        .pauseFor(500)
         .typeString("<br>")
         .typeString("<br>")
         .typeString(
             '<span class="purple">class</span> <span class="tan">Developer</span><span class="parentheses">(</span><span class="tan">human</span>.<span class="light_tan">Human</span><span class="parentheses">)</span>:'
         )
+        .pauseFor(500)
         .typeString("<br>")
         .typeString(
             '<span class="indent-once"><span class="purple">def __init__</span></span>(<span class="light_blue">self</span>, <span class="light_blue">name</span>):'
         )
+        .pauseFor(500)
         .typeString("<br>")
         .typeString(
             '<span class="indent-twice"><span class="tan">super</span>().<span class="purple">__init__</span>(<span class="light_blue">name</span>)</span>'
@@ -36,6 +39,7 @@ $(document).ready(function() {
         .typeString(
             '<span class="light_blue">get_human</span> = <span class="tan">Developer</span>(<span class="string">"Dolsity"</span>)'
         )
+        .pauseFor(500)
         .typeString("<br>")
         .typeString("<br>")
         .typeString('<span class="grey">print</span>(<span class="light_blue">get_human</span>.<span class="grey">greeting</span><span class="tan">()</span>)')
